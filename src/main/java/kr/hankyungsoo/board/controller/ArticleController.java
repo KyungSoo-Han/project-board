2b131f2e004153f9cc02fdab4f4844f298ec641d
kyungsoo han <hanks6160@gmail.com>
2023-07-24 01:34:24 +0900

    #17 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한번에 적용

--- src/main/java/kr/hankyungsoo/board/controller/ArticleController.java
+++ src/main/java/kr/hankyungsoo/board/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package kr.hankyungsoo.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,6 +18,13 @@ public class ArticleController {
     public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map){
+        map.addAttribute("article", "article"); // TODO: 실제 데이터를 넣어야 한다.(임시)
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 
 
