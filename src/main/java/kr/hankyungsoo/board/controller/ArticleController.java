2af7ece12bba8a9ce5acfcff3c55d32a92875c09
kyungsoo han <hanks6160@gmail.com>
2023-07-23 21:28:02 +0900

    #17 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 작성

---
+++ src/main/java/kr/hankyungsoo/board/controller/ArticleController.java
@@ -0,0 +1,13 @@
+package kr.hankyungsoo.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+
+
+}
